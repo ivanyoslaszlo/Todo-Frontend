aef09717ed01b3281d87e7f1dc627f73c0425158
IvanyosLaszlo <ivanyoslaszlo25@gmail.com>
2025-10-11 21:19:17 +0200

    refactor: update logout endpoint call

--- js/config.js
+++ js/config.js
@@ -1,1 +1,14 @@
-const url = "http://localhost:8080/api";
+const isLocalhost = 
+window.location.hostname === "localhost" ||
+ window.location.hostname === "127.0.0.1";
+
+let url;
+  
+if(isLocalhost)
+{
+    url="http://localhost:8080/api";
+}
+else
+{
+    url="/api";
+}
